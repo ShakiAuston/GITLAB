11449bf60aca20cd470d3c1b39676f40458e865c
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2023-04-24 01:32:26 -0400

    deprecation due to file rename
    user.php should be user_profile.php

--- user.php
+++ user.php
@@ -1,4 +1,5 @@
 <head> <link rel="stylesheet" href="CalendarTheme.css">
+// DEPRECATED AS OF 4/24/2023
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
