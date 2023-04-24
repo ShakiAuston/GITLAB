c2128bf795fe97b14c634ffb55cec81dfe17c6a7
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2023-04-24 02:16:24 -0400

    Update header.php
    logo button now returns to index.php

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 <body>
         <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="javascript:void(0)"><img src="./img/logo.png" width="50" height="50" title="Logo" alt="Logo" /></a>
+            <a class="navbar-brand" href="index.php"><img src="./img/logo.png" width="50" height="50" title="Logo" alt="Logo" /></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
                 <span class="navbar-toggler-icon"></span>
             </button>
