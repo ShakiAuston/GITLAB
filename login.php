01ca9da1a6cddb4b45e56946cbbe7db7fb94a624
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2023-04-24 04:05:50 -0400

    added code to login.php
    This is a form that allows the user to login

--- login.php
+++ login.php
@@ -1,1 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
 
+<?php include 'header.php'; ?>
+  
+<div class="container w-75 mt-3">
+  <h3>Sign Up Form to join the website</h3>
+  <p>Please complete the form:</p>
+  
+  <form method="post" action="process_form.php">
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name">
+    <input type="submit" value="Submit">
+  </form>
+
+  <form name="login" action="loginAction.php" class="was-validated">
+    <div class="mb-3">
+      <label for="uname" class="form-label">Username:</label>
+      <input class="form-control" placeholder="Enter username" name="uname" required>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Username Error.</div>
+    </div>
+    <div class="mb-3">
+      <label for="pwd" class="form-label">Password:</label>
+      <input type="password" class="form-control" placeholder="Enter password" name="pswd" required>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Password Error.</div>
+    </div>
+  </form>
+</div>
+
+</body>
+</html>
